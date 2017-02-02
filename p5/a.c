48add36d4636edbb9685af7088a219c144b5b3b8
dasher31415 <dragomirmnikolic@gmail.com>
2017-02-02 18:50:23 -0500

    finished p5

--- p5/a.c
+++ p5/a.c
@@ -1,35 +1,21 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <math.h>
 
-int isPal(int n) {
-  int res[6];
-  int i,j=0;
-  while (n>0) {
-    res[j] = n%10; 
-    n = n/10;
-    j++;
-  }
-  n=j--;
-
- for (i=0; i<=n/2; i++,j--) {
-  if (res[i] != res[j]) return(0);
- }
-
- return(1);
-}
- 
-
 int main (void) {
-  int i, j;
-  int prod=0, res=0;
+  unsigned int i, j, k, res=0;
 
-  for (i=1; i<1000; i++) {
-    for (j=1; j<1000; j++) {
-      prod=i*j;
-      if (isPal(prod) && prod>res) res=prod;
-
-    }
+  for (i=2520; i<2^32; i++) {
+    k=0;
+    for (j=1; j<=20; j++) 
+      if (i%j == 0) k++;
+      if (k==20) {
+	res = i;
+  	printf("%i\n", res);
+	exit(0);
+     }
   }
-  printf("%i\n", res);
+  printf("Number not found ...\n");
+
 }
 
